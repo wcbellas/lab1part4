4d80775d80788e30ef7f79d0a465729a07a1b0cd
william <william.c.bellas@gmail.com>
2015-01-28 19:34:35 -0800

    first try

---
+++ src/carpetdriver/CarpetDriver.java
@@ -0,0 +1,21 @@
+
+package carpetdriver;
+import java.util.Scanner;
+public class CarpetDriver {
+
+
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+        
+        RoomCarpet theCarpet;
+        theCarpet = new RoomCarpet();
+        theCarpet.theDimensions = new RoomDimension();
+        System.out.println("Enter Height:");
+        theCarpet.theDimensions.roomLength=in.nextInt();
+        System.out.println("Enter Width:");
+        theCarpet.theDimensions.roomWidth=in.nextInt();
+        System.out.println("The cost is: "+theCarpet.getCost()+"$");
+    }
+    
+}
